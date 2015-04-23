2c62b93d6daaaa14c8bbb1e23f19b1a3dda9d6c9
ngleadall <nick.gleadall@googlemail.com>
2015-04-23 20:57:32 +0100

    worked on stanford parsing script

--- NG_work/Projects/unclipping_analysis/unclip_analysis.py
+++ NG_work/Projects/unclipping_analysis/unclip_analysis.py
@@ -31,14 +31,14 @@ def system_call( step_name, cmd ):
 
     except subprocess.CalledProcessError as scall:
         if ( VERBOSE == "DEBUG"):
-            print "Script failed at %s stage - exit code was %s, ouput = %s" % (step_name, scall.returncode, scall.output) 
+            print "Script failed at %s stage - exit code was %s, ouput = %s" % (step_name, scall.returncode, scall.output)
 
         if ( VERBOSE == "INFO"):
             print "Script failed at %s stage - exit code was %s" % (step_name, scall.returncode)
         exit()
 
 #------------------------------------------------------------
-# Define fastq's and get sample names. 
+# Define fastq's and get sample names.
 
 # Gets location of fastq
 fastq_dir = sys.argv[1]
@@ -46,7 +46,7 @@ fastq_dir = sys.argv[1]
 # Make analysis directory
 analysis_dir = fastq_dir +"unclip_analysis/"
 
-# Set globals 
+# Set globals
 sample_name = ""
 sample_name_unclipped = ""
 
@@ -66,8 +66,8 @@ for item in os.listdir(fastq_dir):
 make_analysis_directory = "mkdir " + analysis_dir
 
 # remove the sequencing adaptors
-remove_adapters_1 = "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT " + fastq_dir + sample_name +".1.fq.gz"
-remove_adapters_2 = "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT " + fastq_dir + sample_name +".2.fq.gz"
+remove_adapters_1 = "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT " + fastq_dir + sample_name +".1.fq.gz > " + analysis_dir + sample_name + "_rm.1.fq"
+remove_adapters_2 = "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT " + fastq_dir + sample_name +".2.fq.gz > " + analysis_dir + sample_name + "_rm.2.fq"
 
 # Align the reads to the reference (run smalt-0.7.6 to see all options)
 smalt_1 = "/software/bin/smalt-0.7.6 map -f samsoft /refs/HIV/K03455_s1k6 " + sample_name + "_ra.1.fq.gz " + sample_name + "_ra.2.fq.gz > " + sample_name + ".sam"
@@ -91,16 +91,16 @@ alignment_fixing_HIV = "/software/packages/HIV-pipeline/scripts/bam_fix_indels.p
 index_fix = "/software/bin/samtools index " + sample_name + "HIV_fixed.bam"
 
 #------------------------------------------------------------
-# Main pipeline 
+# Main pipeline
 
 system_call("Making directory", make_analysis_directory)
 
 
-#system_call("Removing adapters from fastq 1" , remove_adapters_1)
+system_call("Removing adapters from fastq 1" , remove_adapters_1)
 system_call("Removing adapters from fastq 2" , remove_adapters_2)
 
-    
-   
+
+
 
 
 #------------------------------------------------------------
